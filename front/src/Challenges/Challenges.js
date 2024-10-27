e4f06e7504577f0f6a3a3e8d8821a5f1b8bf62d2
knyazevseven@gmail.com <knyazevseven@gmail.com>
2024-10-27 08:10:28 +0300

    add new changes fix space between

--- front/src/Challenges/Challenges.js
+++ front/src/Challenges/Challenges.js
@@ -3,7 +3,7 @@ import ChallengeCard from './ChallengeCard';
 
 const Challenges = ({ challenges }) => {
   return (
-    <div className="flex wrap gap-4">
+    <div className="flex flex-wrap gap-6 px-4">
       {challenges.map((challenge) => (
         <a href={challenge.id ? `/challenge/${challenge.id}/posts` : ''}>
         <ChallengeCard key={challenge.id} challenge={challenge} />
