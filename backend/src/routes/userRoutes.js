d5edd1dd21fb4785937d49d3d53b68c72dfdb613
knyazevseven@gmail.com <knyazevseven@gmail.com>
2024-10-27 08:28:07 +0300

    fix bug

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middlewares/authMiddleware");
-const route = express.Router();
+const router = express.Router();
 
 //Регистрация пользователя
-route.post("/register", userController.register);
+router.post("/register", userController.register);
 //Авторизация пользователя
-route.post("/login", userController.login);
+router.post("/login", userController.login);
 //Поиск пользователя по ID
-route.get("/findUser/:id", userController.findById);
+router.get("/findUser/:id", userController.findById);
 
 //Поиск всех пользователей
-route.get("/findAllUser", userController.findAll);
+router.get("/findAllUser", userController.findAll);
 
 // Экспорт маршрутизатора
 module.exports = router;
